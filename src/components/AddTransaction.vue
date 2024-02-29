0731a0557f827c9984998ab657f0f554f39c17f3
lum-jashari <lumijashari@hotmail.com>
2024-02-29 14:22:20 +0100

    binded form data to refs

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,18 +1,38 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const onSubmit = () => {
+    console.log(text.value, amount.value);
+};
+
+const text = ref("");
+const amount = ref("");
+</script>
 
 <template>
     <h3>Add new transaction</h3>
-    <form id="form">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..." />
+            <input
+                type="text"
+                id="text"
+                placeholder="Enter text..."
+                v-model="text"
+            />
         </div>
         <div class="form-control">
-            <label for="amount"
-                >Amount <br />
-                (negative = expense, positive = income)</label
-            >
-            <input type="number" id="amount" placeholder="Enter amount..." />
+            <label for="amount">
+                Amount
+                <br />
+                (negative = expense, positive = income )
+            </label>
+            <input
+                type="text"
+                id="amount"
+                placeholder="Enter amount..."
+                v-model="amount"
+            />
         </div>
         <button class="btn">Add transaction</button>
     </form>
