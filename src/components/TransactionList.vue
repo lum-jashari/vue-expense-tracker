e8f10b9732ea0433f60f2bf58af5903a61b89043
lum-jashari <lumijashari@hotmail.com>
2024-02-29 13:45:52 +0100

    expense array loop rendering and UI

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,28 @@
-<script setup></script>
+<script>
+export default {
+    data() {
+        return {
+            transactions: [
+                { id: 1, text: "flower", amount: -19.99 },
+                { id: 2, text: "salary", amount: 299.97 },
+                { id: 3, text: "book", amount: -10 },
+                { id: 4, text: "camera", amount: 150 },
+            ],
+        };
+    },
+};
+</script>
 
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Paycheck <span>$800</span><button class="delete-btn">x</button>
+        <li
+            v-for="item in transactions"
+            :key="item.id"
+            :class="item.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ item.text }} <span>${{ item.amount }}</span>
+            <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
