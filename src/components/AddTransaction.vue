e56c0a6ce0587dc4de794606c09d1a49a4fc6275
lum-jashari <lumijashari@hotmail.com>
2024-02-29 14:34:16 +0100

    added form validation and toasting

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { ref } from "vue";
-
-const onSubmit = () => {
-    console.log(text.value, amount.value);
-};
+import { useToast } from "vue-toastification";
 
 const text = ref("");
 const amount = ref("");
+
+const toast = useToast();
+
+const onSubmit = () => {
+    if (!text.value || !amount.value) {
+        toast.error("Both fields must be filled");
+        return;
+    }
+    console.log(text.value, amount.value);
+    text.value = "";
+    amount.value = "";
+};
 </script>
 
 <template>
